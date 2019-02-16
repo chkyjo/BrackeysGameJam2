909b674bd957e7ada4886ddf7d19a09dbdd0cc62
Chris <chkyjo@gmail.com>
2019-02-16 01:10:47 -0800

    Main menu, main scene

---
+++ BrackeysGameJam2/Assets/SceneLoader.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour{
+
+    public GameObject loadingPanel;
+    public Slider loadingBar;
+    public Text completionText;
+
+    public void LoadLevel(int sceneIndex) {
+        StartCoroutine(LoadAsync(sceneIndex));
+    }
+
+    IEnumerator LoadAsync(int sceneIndex) {
+
+        AsyncOperation load = SceneManager.LoadSceneAsync(sceneIndex);
+        loadingPanel.SetActive(true);
+        float progress;
+
+        while (!load.isDone) {
+            progress = Mathf.Clamp01(load.progress / 0.9f);
+            loadingBar.value = progress;
+            completionText.text = progress * 100f + "%";
+
+            yield return null;
+        }
+
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+}
